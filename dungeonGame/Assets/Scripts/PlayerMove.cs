d35031c64d4c3341222989bafcd36f0b41c22978
VolkenK <volkenkoelewijn@gmail.com>
2018-10-14 16:21:38 +0200

    move script gemaakt en aan shoot script gestart

---
+++ dungeonGame/Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour {
+
+    private Rigidbody rigidBody;
+    [SerializeField]private float speed = 1;
+
+	// Use this for initialization
+	void Start () {
+        rigidBody = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 force = new Vector3();
+        force.x = Input.GetAxis("Horizontal");
+        force.z = Input.GetAxis("Vertical");
+        force *= speed;
+        rigidBody.AddForce(force);
+        
+	}
+}
---
+++ dungeonGame/Assets/Scripts/PlayerShoot.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShoot : MonoBehaviour {
+
+   
+
+    public GameObject Bullet;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 direction = new Vector3();
+        if (Input.GetKey("North"))
+        {
+            direction.z += 1;
+
+        }
+
+    }
+}
